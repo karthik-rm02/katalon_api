0f25ec245536850d4497d265995e63e71aa9942e
karthik-rm02 <aakashshaji_rm02@outlook.com>
2025-02-22 20:57:09 +0530

    pet test cases

--- Object Repository/petstore_json/addPet.rs
+++ Object Repository/petstore_json/addPet.rs
@@ -7,12 +7,25 @@
    <selectorMethod>BASIC</selectorMethod>
    <smartLocatorEnabled>false</smartLocatorEnabled>
    <useRalativeImagePath>false</useRalativeImagePath>
-   <autoUpdateContent>true</autoUpdateContent>
-   <connectionTimeout>-1</connectionTimeout>
+   <authorizationRequest>
+      <authorizationInfo>
+         <entry>
+            <key>bearerToken</key>
+            <value>test</value>
+         </entry>
+      </authorizationInfo>
+      <authorizationType>Bearer</authorizationType>
+   </authorizationRequest>
+   <autoUpdateContent>false</autoUpdateContent>
+   <connectionTimeout>0</connectionTimeout>
    <followRedirects>false</followRedirects>
    <httpBody></httpBody>
-   <httpBodyContent></httpBodyContent>
-   <httpBodyType></httpBodyType>
+   <httpBodyContent>{
+  &quot;text&quot;: &quot;{\n  \&quot;id\&quot;: ${id},\n  \&quot;category\&quot;: {\n    \&quot;id\&quot;: 0,\n    \&quot;name\&quot;: \&quot;string\&quot;\n  },\n  \&quot;name\&quot;: \&quot;${name}\&quot;,\n  \&quot;photoUrls\&quot;: [\n    \&quot;string\&quot;\n  ],\n  \&quot;tags\&quot;: [\n    {\n      \&quot;id\&quot;: 0,\n      \&quot;name\&quot;: \&quot;string\&quot;\n    }\n  ],\n  \&quot;status\&quot;: \&quot;available\&quot;\n}&quot;,
+  &quot;contentType&quot;: &quot;application/json&quot;,
+  &quot;charset&quot;: &quot;UTF-8&quot;
+}</httpBodyContent>
+   <httpBodyType>text</httpBodyType>
    <httpHeaderProperties>
       <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
@@ -20,14 +33,6 @@
       <type>Main</type>
       <value>application/json</value>
       <webElementGuid>219ceb9d-4416-4338-ae1a-9d59ccefe605</webElementGuid>
-   </httpHeaderProperties>
-   <httpHeaderProperties>
-      <isSelected>true</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>Content-type</name>
-      <type>Main</type>
-      <value>application/xml</value>
-      <webElementGuid>484ec12d-6ed5-4899-b617-3452c5228b79</webElementGuid>
    </httpHeaderProperties>
    <httpHeaderProperties>
       <isSelected>true</isSelected>
@@ -40,13 +45,13 @@
    <httpHeaderProperties>
       <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
-      <name>Accept</name>
+      <name>Authorization</name>
       <type>Main</type>
-      <value>application/xml</value>
-      <webElementGuid>ebed784a-ce3b-4c28-a8be-a114949b99ca</webElementGuid>
+      <value>Bearer test</value>
+      <webElementGuid>0dec7cf5-fd24-42b9-a65a-b331202a38f0</webElementGuid>
    </httpHeaderProperties>
    <katalonVersion>10.1.0</katalonVersion>
-   <maxResponseSize>-1</maxResponseSize>
+   <maxResponseSize>0</maxResponseSize>
    <migratedVersion>5.4.1</migratedVersion>
    <path>/pet</path>
    <restRequestMethod>POST</restRequestMethod>
@@ -57,7 +62,48 @@
    <soapRequestMethod></soapRequestMethod>
    <soapServiceEndpoint></soapServiceEndpoint>
    <soapServiceFunction></soapServiceFunction>
-   <socketTimeout>-1</socketTimeout>
+   <socketTimeout>0</socketTimeout>
    <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <validationSteps>
+      <id>991faad1-93c6-4d76-8699-f50cca28e9ea</id>
+      <name>test1</name>
+      <type>JSON_SCHEMA</type>
+      <dataType>FILE</dataType>
+      <target>RESPONSE</target>
+      <data>C:\Users\aakas\Katalon Studio\Test_API\addPet.json</data>
+      <activate>true</activate>
+   </validationSteps>
+   <variables>
+      <defaultValue>5</defaultValue>
+      <description></description>
+      <id>e272fbca-8ba1-455b-920a-411092d770ed</id>
+      <masked>false</masked>
+      <name>id</name>
+   </variables>
+   <variables>
+      <defaultValue>'test'</defaultValue>
+      <description></description>
+      <id>d55826f7-48a9-4f6d-b3eb-f0d630d0ec35</id>
+      <masked>false</masked>
+      <name>name</name>
+   </variables>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+
+
+WS.verifyResponseStatusCode(response, 200)
+
+assertThat(response.getStatusCode()).isEqualTo(200)</verificationScript>
    <wsdlAddress></wsdlAddress>
 </WebServiceRequestEntity>
