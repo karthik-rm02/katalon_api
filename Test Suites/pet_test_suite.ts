78a4f8e70f90fdc40c089dc838844cc748195474
karthik-rm02 <aakashshaji_rm02@outlook.com>
2025-02-22 23:17:37 +0530

    test suite updates

--- Test Suites/pet_test_suite.ts
+++ Test Suites/pet_test_suite.ts
@@ -16,7 +16,7 @@
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/petstore/TC001_newPet</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
       <variableLink>
          <testDataLinkId></testDataLinkId>
          <type>DEFAULT</type>
